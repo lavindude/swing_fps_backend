2805b5eb4f03658a9760e270b98c2b9564c0f153
lavindude <lavindude@gmail.com>
2022-02-15 15:01:16 -0800

    boiler plate initialized

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
-const app = express()
+const { typeDefs } = require('./Schema/TypeDefs')
+const { resolvers } = require('./Schema/Resolvers')
 
+const app = express()
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.applyMiddleware({ app })
+app.listen({port: 4000}, () => {
+    console.log("Listening")
+})
